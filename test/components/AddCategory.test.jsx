31ed48a5e16cf16c1298d1bfbd513709172a311b
hspdaw <hspdaw@gmail.com>
2023-03-21 21:36:30 +0100

    Simular submit del formulario

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -13,4 +13,18 @@ describe('Pruebas en <AddCategory/>', () =>{
         //screen.debug();
     })
 
+
+    test('debe llamar onNewCategory si el input tiene un valor',()=>{
+       
+        const inputValue = 'Saitama';
+        render(<AddCategory onNewCategory={()=>{}}/>);
+        
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input,{ target:{ value: inputValue}});
+        fireEvent.submit( form );
+
+        expect ( input.vlaue ).toBe('');
+    })
 })
